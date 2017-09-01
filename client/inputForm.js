7d6db846955d4824c6060dc8f43997df74e205f2
Nikita Staroseltsev <nikitozz_13@mail.ru>
2017-09-01 05:39:02 +0300

    Lesson 5: Simple create form button event

--- client/inputForm.js
+++ client/inputForm.js
@@ -1,3 +1,8 @@
 Template.inputForm.onCreated(function() {
   setHeader({title: 'Создание..', isBackVisible: true})
 });
+Template.inputForm.events({
+  'click .js-submit': function() {
+    console.log('It works!');
+  }
+});
