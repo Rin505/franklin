a1e65f4110da694039cfed488b78b25852268e55
Nikita Staroseltsev <nikitozz_13@mail.ru>
2017-09-01 16:08:34 +0300

    Lesson 6: Show goodness progress

--- client/goodnessList.js
+++ client/goodnessList.js
@@ -1,6 +1,16 @@
 Template.goodnessList.helpers({
   'goodnessList': function() {
     return Goodness.find({}, {sort: {date:-1}});
+  },
+  'getProgress': function(id) {
+    var maxCount = Goodness.findOne(id).maxCount;
+    var currentDay = moment().format('DD-MM-YYYY');
+    var counterDocument = Counters.findOne({goodnessId: id, date: currentDay});
+    if (counterDocument) {
+      return 100 - (counterDocument.counter / maxCount) * 100;
+    } else {
+      return 100;
+    }
   }
 });
 Template.goodnessList.onCreated(function() {
