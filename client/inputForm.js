e4baa738312d6b7004c6be0f74c8896c627ccd27
Nikita Staroseltsev <nikitozz_13@mail.ru>
2017-09-03 19:11:04 +0300

    Lesson 11: Add images to goodness cards

--- client/inputForm.js
+++ client/inputForm.js
@@ -5,7 +5,8 @@ Template.inputForm.events({
   'click .js-submit': function() {
     var text = $('.js-text').val()
     if (text.length > 0) {
-      Goodness.insert({title: text, date: new Date, maxCount: 0});
+      var fileName = Math.floor(Math.random() * 10 + 1) + '.jpg';
+      Goodness.insert({title: text, date: new Date, maxCount: 0, avatar: fileName});
       Router.go('goodnessList');
     }
   }
